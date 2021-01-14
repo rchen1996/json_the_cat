3b2952d6224e8219264c5d520b60b971449d300f
Rebecca Chen <rebecca061996@gmail.com>
2021-01-14 20:44:36 +0000

    refactored code to change to a function and exported

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,22 @@
 const request = require('request');
-const breedName = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
+// need to include callback because async
+const fetchBreedDescription = function(breed, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+    } else {
+      // body is a JSON string - need to turn it into an object
+      const data = JSON.parse(body);
+      // data is an array
+      if (data.length === 0) {
+        error = "The requested breed was not found";
+        callback(error);
+      } else {
+        callback(error, data[0].description);
+      }
+    }
+  });
+};
 
-  // body is a JSON string - need to turn it into an object
-  const data = JSON.parse(body);
-  // data is an array
-
-  if (data.length === 0) {
-    console.log("The requested breed was not found");
-  } else {
-    console.log(data[0].description);
-  }
-});
+module.exports = { fetchBreedDescription };
